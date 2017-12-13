630fe804d6de4db841e55b4e840597a2fa988cc4
forgehe <liao098@gmail.com>
2017-12-13 00:41:16 -0800

    Importing all files

---
+++ includes/playVid.js
@@ -0,0 +1,20 @@
+function playVid(){
+var vid = document.getElementById("bgvid");
+		vid.play();
+// There is no stopping the meme train CHOOO CHOOOO
+};
+
+window.onload = function() {
+    var backgroundAudio=document.getElementById("bgvid");
+    backgroundAudio.volume=0.7;
+}
+
+/*
+
+  __  __  ____    __  __  ____       ____    __  _____  ______  
+ |  |/ / |    \  |  |/ / |    \     |    \  /  |/     \|   ___| 
+ |     \ |     \ |     \ |     \  _ |     \/   ||     ||   ___| 
+ |__|\__\|__|\__\|__|\__\|__|\__\|_||__/\__/|__|\_____/|______| 
+                                                                
+
+ */
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>GetOutOfMyHead</title>
+
+<?php 
+
+$metaKeywords = 'doki doki literature club, ddlc, sayori';
+require('includes/head.html');
+
+?>
+<link rel="stylesheet" type="text/css" href="includes/style.videopage.css">
+<script async="true" src="includes/playVid.js"></script>
+
+</head>
+
+
+<body>
+	<?php
+
+		require('includes/body-sayori.html');
+
+	?>
+	
+</body>
+</html>
