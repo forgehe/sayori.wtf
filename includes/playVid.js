6d3fa0f88ad116448c8813c54a9620749037bd6d
forgehe <liao098@gmail.com>
2021-02-16 05:16:21 +0000

    VPS changes

--- includes/playVid.js
+++ includes/playVid.js
@@ -1,7 +1,10 @@
 function playVid(){
 var vid = document.getElementById("bgvid");
-		vid.play();
-// There is no stopping the meme train CHOOO CHOOOO
+	vid.play();
+    if(vid.muted) {
+        vid.muted = false;
+    }
+// There is no muting the meme train CHOOO CHOOOO
 };
 
 window.onload = function() {
@@ -11,10 +14,11 @@ window.onload = function() {
 
 /*
 
-  __  __  ____    __  __  ____       ____    __  _____  ______  
- |  |/ / |    \  |  |/ / |    \     |    \  /  |/     \|   ___| 
- |     \ |     \ |     \ |     \  _ |     \/   ||     ||   ___| 
- |__|\__\|__|\__\|__|\__\|__|\__\|_||__/\__/|__|\_____/|______| 
-                                                                
+       _           _     __  __             _ _         
+      | |         | |   |  \/  |           (_) |        
+      | |_   _ ___| |_  | \  / | ___  _ __  _| | ____ _ 
+  _   | | | | / __| __| | |\/| |/ _ \| '_ \| | |/ / _` |
+ | |__| | |_| \__ \ |_  | |  | | (_) | | | | |   < (_| |
+  \____/ \__,_|___/\__| |_|  |_|\___/|_| |_|_|_|\_\__,_|
 
- */
+*/
